75a72d35d63c000f0c0a66a4d7fb5f0377215054
Dev_Abdo_A_Mo <113772803+abdoahmed2004@users.noreply.github.com>
2025-05-11 21:17:16 +0300

    Add files via upload

---
+++ My-api/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify
+import pymysql
+
+app = Flask(__name__)
+
+
+conn = pymysql.connect(
+    host='sql12.freesqldatabase.com',
+    user='sql12778098',
+    password='ck3faGpWQK',
+    database='sql12778098',
+    port=3306
+)
+
+@app.route('/students')
+def get_students():
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT * FROM students")
+        result = cursor.fetchall()
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run()
